72919956e02dbdb9d4801d4d9874dd513d677075
Lucas Batista <lucasq04@gmail.com>
2023-02-01 19:23:18 -0300

    exception handling

--- src/main/java/com/lucasbatista/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasbatista/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.lucasbatista.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasbatista.workshopmongo.domain.User;
+import com.lucasbatista.workshopmongo.dto.UserDTO;
 import com.lucasbatista.workshopmongo.service.UserService;
 
 @RestController
@@ -19,9 +22,16 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = userService.findById(id);
+		UserDTO userDto = new UserDTO(obj);
+		return ResponseEntity.ok().body(userDto);
+	}
 }
--- src/main/java/com/lucasbatista/workshopmongo/service/UserService.java
+++ src/main/java/com/lucasbatista/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.lucasbatista.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasbatista.workshopmongo.domain.User;
 import com.lucasbatista.workshopmongo.repository.UserRepository;
+import com.lucasbatista.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String Id) {
+		Optional<User> obj = userRepository.findById(Id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 }
